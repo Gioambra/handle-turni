7e8420d5f3cd8271d3f3b7ecbd24ccfa8fc200a1
Gambra <droneitaly.ga@gmail.com>
2021-10-27 13:28:31 +0200

    add date-fns

--- turni/pages/turni.jsx
+++ turni/pages/turni.jsx
@@ -1,5 +1,11 @@
-const  turni = ()  =>{
-    return <div>turni!!!</div>
-  }
-  
-  export default turni
+import {format} from "date-fns";
+const turni = () => {
+    const getTodayWithNoTime = () => {
+        return format(new Date(), "yyyy-MM-dd");
+    };
+    // get today and go away of 7 days
+    console.log(getTodayWithNoTime());
+    return <div>turni!!!</div>;
+};
+
+export default turni;
