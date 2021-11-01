46761058185b74376199dbd8b6aa7ba2e8cea852
Gioambra <droneitaly.ga@gmail.com>
2021-11-01 12:07:15 +0100

    Create function current week

--- turni/pages/handlingDate.js
+++ turni/pages/handlingDate.js
@@ -1,14 +1,32 @@
-import {format, addDays, parseISO} from "date-fns";
-import {it} from "date-fns/locale";
+import {format, addDays} from "date-fns";
 const getTodayWithNoTime = () => {
     console.log(format(new Date(), "dd-MM-yyyy"));
     return format(new Date(), "dd-MM-yyyy");
 };
 
 //TODO formattare in data europea e automaticamente capire giorno me se anno
+
+// input ->> data di oggi
+// output->> data di domani
 const getNextDay = (from) => {
-    console.log(addDays(new Date(2021, 10, 27), 1));
-    return addDays(new Date(2021, 10, 27), 1);
+    const dateFrom = from.split('-');
+    console.log(format(addDays(new Date(dateFrom[2], (dateFrom[1] - 1), dateFrom[0]), 1),'dd-MM-yyyy'));
+    // aano -mese -giorno
+    return format(addDays(new Date(dateFrom[2], (dateFrom[1] - 1), dateFrom[0]), 1),'dd-MM-yyyy');
 };
 
-export {getTodayWithNoTime, getNextDay};
+// input ->>data di oggi 
+// output->> tutta la settimana corrente
+const getCurrentWeek = (from) =>
+{
+   let tomorrow = getNextDay(from);
+    let currentWeek = [];
+    for(var index = 0; index<7; index++)
+    {
+        currentWeek.push(tomorrow);
+        tomorrow = getNextDay(tomorrow)
+    }
+    console.log(currentWeek);
+    return currentWeek
+}
+export {getTodayWithNoTime, getNextDay, getCurrentWeek};
--- turni/pages/turni.jsx
+++ turni/pages/turni.jsx
@@ -1,11 +1,13 @@
-import {getTodayWithNoTime, getNextDay} from "./handlingDate";
+import {getTodayWithNoTime, getNextDay, getCurrentWeek} from "./handlingDate";
 const turni = () => {
     // get today and go away of 7 days
     const today = getTodayWithNoTime();
+    //console.log(getNextDay(today));
+
+    console.log(getCurrentWeek(today));
     // get 7 days from today
     // creare una funziona che mi dia le prossime 7 date a partire da oggi
-    console.log(getNextDay("test"));
-    return <div>a </div>;
+    return <div>{today}</div>;
 };
 
 export default turni;
