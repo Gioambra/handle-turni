3c63f056887e56357cf48c2d69c742b1b77e8145
Gioambra <droneitaly.ga@gmail.com>
2021-11-05 21:51:15 +0100

    initial data inserting

--- sql/create.sql
+++ sql/create.sql
@@ -14,3 +14,11 @@ CREATE TABLE users (
     email text,
     phoneNumber integer,
 );
+
+-- initial data 
+INSERT INTO currentWeek (ID,current_Day, user) VALUES ('06-11-2021','2021-11-06',1);
+INSERT INTO currentWeek (ID,current_Day, user) VALUES ('06-11-2021','2021-11-06',2);
+INSERT INTO currentWeek (ID,current_Day, user) VALUES ('06-11-2021','2021-11-06',3);
+INSERT INTO users (user_id,user_name, email,phoneNumber) VALUES (1,'John','john@email',12132);
+INSERT INTO users (user_id,user_name, email,phoneNumber) VALUES (2,'Luke','luke@email',02132);
+INSERT INTO users (user_id,user_name, email,phoneNumber) VALUES (3,'bernard','bernard@email',12132);
